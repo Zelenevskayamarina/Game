fd6787522246554aee2fa70f09f0e18ae8d70a01
Marina Zelenevskaya <mzelianeuskaya@yandex.ru>
2022-11-30 02:41:32 +0300

    feat: add project1 and project2 for game task

---
+++ js/spriteAnimations.js
@@ -0,0 +1,25 @@
+spriteAnimations = [
+    "idle" = {
+        width: 525,
+        height: 523, 
+        loc: [
+            {x: 0, y: 0},
+            {x: 575, y: 0},
+            {x: 1150, y: 0},
+            {x: 1725, y: 0},
+            {x: 2300, y: 0},
+            {x: 2875, y: 0},
+            {x: 3450, y: 0},
+        ]
+    },
+    "jump" = {
+        width: 120,
+        height: 120, 
+        loc: []
+    },
+    "run" = {
+        width: 1200,
+        height: 1250, 
+        loc: []
+    },
+]
